64ecc6f85f03720b01e8605b02a3bb9f186d5e96
Sagarhajrat <84784446+Sagarhajrat@users.noreply.github.com>
2021-11-11 20:59:12 +0530

    Update Analine C++ Code by Md Sagar Khan_Artificial Neural Networks.cpp

--- Analine C++ Code by Md Sagar Khan_Artificial Neural Networks.cpp
+++ Analine C++ Code by Md Sagar Khan_Artificial Neural Networks.cpp
@@ -1,11 +1,4 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
+/*C++ code to train given fucntion using analine netowrk_created by Md Sagar Khan*/
 #include <iostream>
 #include<cmath>
 
